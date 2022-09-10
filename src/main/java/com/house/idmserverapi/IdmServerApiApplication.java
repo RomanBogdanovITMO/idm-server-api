98c3bb45e0b25e5601fd88bbf880397ca17ac891
RomanBogdanovITMO <quke@inbox.ru>
2022-09-10 19:51:14 +0300

    Initial commit

---
+++ src/main/java/com/house/idmserverapi/IdmServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.house.idmserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdmServerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/house/idmserverapi/IdmServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.idmserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmServerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
